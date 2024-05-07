2acbab6b5de6262b65bf9b8d5e7709231c737782
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-05-07 20:39:19 +0530

    tabs-update

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -11,7 +11,7 @@ const Feature = () => {
   };
 
   return (
-    <div className="py-lg-5 mb-31 position-relative">
+    <div className="py-lg-5 mb-31 position-relative ">
       <div className="container pt-3 pt-md-5 mt-2">
         <button className="mx-auto feature_btn mb-2 fs-sm fw-semibold ff-sans lh-24 off-green d-flex justify-content-center">
           OUR FEATURES
@@ -23,48 +23,50 @@ const Feature = () => {
         <p className="fs-sm fw-normal ff-sans off-gray lh-25 text-center mb-md-5">
           Your path to wellness, Tailored down to your DNA
         </p>
-        <div className="d-flex align-items-center justify-content-center pt-2 mt-1 flex-wrap gap-25">
-          <a
-            onClick={() => handleTabClick("tab1")}
-            className={`tab_btn  font-gradient text-decoration-none fs-sm fw-semibold  lh-25 ff-sans d-flex justify-content-center align-items-center ${
-              activeTab === "tab1" ? "tab_1" : ""
-            }`}
-            href="#"
-          >
-            DNA-customized nutrition
-          </a>
-          <a
-            onClick={() => handleTabClick("tab2")}
-            className={`tab_btn  font-gradient text-decoration-none fs-sm fw-semibold  lh-25 ff-sans d-flex justify-content-center align-items-center ${
-              activeTab === "tab2" ? "tab_1" : ""
-            }`}
-            href="#"
-          >
-            Fitness that fits you
-          </a>
-          <a
-            onClick={() => handleTabClick("tab3")}
-            className={`tab_btn  text-decoration-none font-gradient fs-sm fw-semibold  lh-25 ff-sans d-flex justify-content-center align-items-center ${
-              activeTab === "tab3" ? "tab_1" : ""
-            }`}
-            href="#"
-          >
-            Mind matters
-          </a>
-          <a
-            onClick={() => handleTabClick("tab4")}
-            className={`tab_btn  text-decoration-none font-gradient  fs-sm fw-semibold lh-25 ff-sans d-flex justify-content-center align-items-center ${
-              activeTab === "tab4" ? "tab_1" : ""
-            }`}
-            href="#"
-          >
-            Wellness, tracked and tailored
-          </a>
+        <div className="overflow-x-auto overflow-x-scroll ">
+          <div className="d-flex align-items-center justify-content-center pt-2 mt-1 w-1000   tabs_gap">
+            <a
+              onClick={() => handleTabClick("tab1")}
+              className={`tab_btn  font-gradient text-decoration-none fs-sm fw-semibold  lh-25 ff-sans d-flex justify-content-center align-items-center flex-nowrap ${
+                activeTab === "tab1" ? "tab_1" : ""
+              }`}
+              href="#"
+            >
+              DNA-customized nutrition
+            </a>
+            <a
+              onClick={() => handleTabClick("tab2")}
+              className={`tab_btn  font-gradient text-decoration-none fs-sm fw-semibold  lh-25 ff-sans d-flex justify-content-center align-items-center flex-nowrap ${
+                activeTab === "tab2" ? "tab_1" : ""
+              }`}
+              href="#"
+            >
+              Fitness that fits you
+            </a>
+            <a
+              onClick={() => handleTabClick("tab3")}
+              className={`tab_btn  text-decoration-none font-gradient fs-sm fw-semibold  lh-25 ff-sans d-flex justify-content-center align-items-center flex-nowrap ${
+                activeTab === "tab3" ? "tab_1" : ""
+              }`}
+              href="#"
+            >
+              Mind matters
+            </a>
+            <a
+              onClick={() => handleTabClick("tab4")}
+              className={`tab_btn  text-decoration-none font-gradient  fs-sm fw-semibold lh-25 ff-sans d-flex justify-content-center align-items-center flex-nowrap ${
+                activeTab === "tab4" ? "tab_1" : ""
+              }`}
+              href="#"
+            >
+              Wellness, tracked and tailored
+            </a>
+          </div>
         </div>
         <div className="row align-items-center justify-content-between pt-4 py-md-5">
-          <div className="col-lg-7 position-relative z-3">
+          <div className="col-lg-7 position-relative z-3 mt-lg-5">
             {activeTab === "tab1" && (
-              <div className="d-flex flex-column justify-content-center justify-content-lg-start align-items-center">
+              <div className="d-flex flex-column justify-content-center justify-content-lg-start  pt-lg-5 mt-lg-5">
                 <h1 className="fs-xl text-center text-md-start fw-bold ff-roboto lh-76 text-black mb-md-3 mb-2">
                   DNA-customized nutrition
                 </h1>
@@ -101,7 +103,7 @@ const Feature = () => {
               </div>
             )}
             {activeTab === "tab2" && (
-              <div className="d-flex flex-column justify-content-center justify-content-lg-start align-items-center">
+              <div className="d-flex flex-column justify-content-center justify-content-lg-start  pt-lg-5 mt-lg-5">
                 <h1 className="fs-xl text-center text-md-start fw-bold ff-roboto lh-76 text-black mb-md-3 mb-2">
                   Fitness that fits you{" "}
                 </h1>
@@ -138,7 +140,7 @@ const Feature = () => {
               </div>
             )}
             {activeTab === "tab3" && (
-              <div className="d-flex flex-column justify-content-center justify-content-lg-start align-items-center">
+              <div className="d-flex flex-column justify-content-center justify-content-lg-start  pt-lg-5 mt-lg-5">
                 <h1 className="fs-xl text-center text-md-start fw-bold ff-roboto lh-76 text-black mb-md-3 mb-2">
                   Mind matters{" "}
                 </h1>
@@ -176,10 +178,25 @@ const Feature = () => {
               </div>
             )}
             {activeTab === "tab4" && (
-              <div className="d-flex flex-column justify-content-center justify-content-lg-start align-items-center">
+              <div className="d-flex flex-column justify-content-center justify-content-lg-start pt-lg-5 mt-lg-5">
                 <h1 className="fs-xl text-center text-md-start fw-bold ff-roboto lh-76 text-black mb-md-3 mb-2">
                   Wellness, tracked and tailored{" "}
                 </h1>
+
+                <div className="d-flex align-items-start mb-md-3 mb-2 gap-14">
+                  <img className="tickmark_svg" src={tickmark} alt="tickmark" />
+                  <p className="fs-sm fw-normal mw_469 ff-sans lh-25 off-gray mb-0">
+                    Deep dive into your DNA: get to the heart of your health
+                    with in-depth genomic insights
+                  </p>
+                </div>
+                <div className="d-flex align-items-start mb-md-3 mb-2 gap-14">
+                  <img className="tickmark_svg" src={tickmark} alt="tickmark" />
+                  <p className="fs-sm fw-normal mw_469 ff-sans lh-25 off-gray mb-0">
+                    Beyond genetics: integrate comprehensive exam results for a
+                    full health picture
+                  </p>
+                </div>
                 <div className="d-flex align-items-start mb-md-3 mb-2 gap-14">
                   <img className="tickmark_svg" src={tickmark} alt="tickmark" />
                   <p className="fs-sm fw-normal mw_469 ff-sans lh-25 off-gray mb-0">
@@ -196,24 +213,10 @@ const Feature = () => {
                     comprehensive roadmap
                   </p>
                 </div>
-                <div className="d-flex align-items-start mb-md-3 mb-2 gap-14">
-                  <img className="tickmark_svg" src={tickmark} alt="tickmark" />
-                  <p className="fs-sm fw-normal mw_469 ff-sans lh-25 off-gray mb-0">
-                    Deep dive into your DNA: get to the heart of your health
-                    with in-depth genomic insights
-                  </p>
-                </div>
-                <div className="d-flex align-items-start mb-md-3 mb-2 gap-14">
-                  <img className="tickmark_svg" src={tickmark} alt="tickmark" />
-                  <p className="fs-sm fw-normal mw_469 ff-sans lh-25 off-gray mb-0">
-                    Beyond genetics: integrate comprehensive exam results for a
-                    full health picture
-                  </p>
-                </div>
               </div>
             )}
           </div>
-          <div className="col-lg-4 d-flex justify-content-center">
+          <div className="col-lg-4 d-flex pt-4 pt-lg-0 justify-content-center">
             <img
               className="mobile_phone me-lg-5 position-relative z-3"
               src={mobile_phone}
